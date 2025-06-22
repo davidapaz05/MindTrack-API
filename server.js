bd1780182d3e938844eed426fcc33a7a0d5417d0
davidapaz05 <davipaz125@gmail.com>
2025-06-22 11:14:04 -0300

    Versão final da API com Docker

--- server.js
+++ server.js
@@ -14,7 +14,28 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+const allowedOrigins = [
+    'https://mind-tracking.vercel.app', // Vercel
+    'http://localhost:5137',           // Sua máquina local
+    'http://127.0.0.1:5137',           // Outra forma local (IP)
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Permite requisições sem origin (ex: mobile, curl)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
